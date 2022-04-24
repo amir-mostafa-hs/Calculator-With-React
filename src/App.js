ed162bcc3218d1a1b0e481759b28294139b369c2
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-04-24 20:55:36 +0430

    add input number code and style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React from "react";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <React.Fragment>
+      <main className="container">
+        <aside className="form">
+          <input
+            type="text"
+            placeholder="0"
+            className="inputNumber"
+            onInput={(evt) => {
+              evt.target.value = evt.target.value
+                .replace(/[^0-9.&-]/g, "")
+                .replace(/(\..*?)\..*/g, "$1");
+            }}
+          />
+        </aside>
+      </main>
+    </React.Fragment>
+  );
 }
 
 export default App;
